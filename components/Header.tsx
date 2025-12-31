cfb97d1a9a2c3a69c2e5d8800aa89a421d9ee599
keithhegit <keith@ogcloud.com>
2025-12-31 16:49:28 +0800

    Fix: Update banner CDN to jpg

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
       <div className="flex items-center flex-1 min-w-0">
         <a href="/" className="flex items-center hover:opacity-90 transition-opacity w-full min-w-0">
           <img 
-            src="https://pub-c98d5902eedf42f6a9765dfad981fd88.r2.dev/page%E4%B8%93%E7%94%A8/ogsprite/og_sprite_sheet_banner.png" 
+            src="https://pub-c98d5902eedf42f6a9765dfad981fd88.r2.dev/page%E4%B8%93%E7%94%A8/ogsprite/og_sprite_sheet_banner.jpg" 
             alt="OgSprite Logo" 
             className="h-20 w-auto max-w-full object-contain"
           />
